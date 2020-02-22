807b47f966153b345e34f4d87cf7e8e31c2ba385
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-02-22 09:14:53 -0300

    Add AmazonCodeFirstDbContext
    This DbContext will be used to connect in a database that the persistence strategy will be mainly code first

---
+++ EntityFrameworkCoreLab.Persistence/EntityFrameworkContexts/AmazonCodeFirstDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts
+{
+    public class AmazonCodeFirstDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=192.168.1.14,32771;Database=AmazonCodeFirst;User ID=sa;Password=sqlserver.252707;
+                                                  Encrypt=True;Trusted_Connection=True;Connection Timeout=3000;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
