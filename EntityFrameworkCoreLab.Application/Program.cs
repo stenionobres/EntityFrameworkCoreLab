30fd6e3326edba8111a95b72f435211f9059375d
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-02-22 08:31:39 -0300

    Create startup solution

---
+++ EntityFrameworkCoreLab.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkCoreLab.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EntityFrameworkCoreLab.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EntityFrameworkCoreLab.Persistence
+{
+    public class Class1
+    {
+    }
+}
