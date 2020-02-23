2752e6303f99c87de027c260d7a6ef93c35c2c4c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-02-23 11:50:51 -0300

    Create Customer DataTransferObject and migration

---
+++ EntityFrameworkCoreLab.Persistence/DataTransferObjects/Amazon/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
