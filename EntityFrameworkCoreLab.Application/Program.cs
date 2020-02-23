28876d9aeee5dc5e2abce8a2eaf524f4e6b6e7e3
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-02-23 11:53:16 -0300

    Save a dummy customer
    To test the AmazonCodeFirstDbContext configuration

--- EntityFrameworkCoreLab.Application/Program.cs
+++ EntityFrameworkCoreLab.Application/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon;
+using EntityFrameworkCoreLab.Persistence.Mappers;
+using System;
 
 namespace EntityFrameworkCoreLab.Application
 {
@@ -7,6 +9,9 @@ namespace EntityFrameworkCoreLab.Application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var customer = new Customer() { Name = "Customer 01" };
+
+            new AmazonCustomerMapper().Save(customer);
         }
     }
 }
---
+++ EntityFrameworkCoreLab.Persistence/Mappers/Amazon/AmazonCustomerMapper.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon;
+using EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts;
+using System;
+
+namespace EntityFrameworkCoreLab.Persistence.Mappers
+{
+    public class AmazonCustomerMapper
+    {
+        public void Save(Customer customer)
+        {
+            try
+            {
+                using (var amazonCodeFirstContext = new AmazonCodeFirstDbContext())
+                {
+                    amazonCodeFirstContext.Customers.Add(customer);
+                    amazonCodeFirstContext.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
