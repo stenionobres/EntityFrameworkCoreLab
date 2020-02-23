8b1d52cab37ae295156608152f028c311680d9a3
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-02-23 20:01:48 -0300

    Add EbayDatabaseFirstDbContext
    This DbContext will be used to connect in a database that the persistence strategy will be mainly database first

---
+++ EntityFrameworkCoreLab.Persistence/DataTransferObjects/Ebay/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
