6a1c543227c19722423a9068f4e750ab7c8fd41b
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-03 09:26:23 -0300

    Add Customer migration to EbayDatabaseFirst database

--- EntityFrameworkCoreLab.Persistence/DataTransferObjects/Ebay/Customer.cs
+++ EntityFrameworkCoreLab.Persistence/DataTransferObjects/Ebay/Customer.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public string Name { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        public string Cpf { get; set; }
     }
 }
--- EntityFrameworkCoreLab.Persistence/EntityFrameworkContexts/EbayDatabaseFirstDbContext.cs
+++ EntityFrameworkCoreLab.Persistence/EntityFrameworkContexts/EbayDatabaseFirstDbContext.cs
@@ -8,6 +8,8 @@ namespace EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts
         private const string ConnectionString = @"Server=192.168.1.14,22331;Database=EbayDatabaseFirst;User ID=sa;Password=sqlserver.252707;
                                                   Encrypt=False;Trusted_Connection=False;Connection Timeout=3000;";
 
+        public DbSet<Customer> Customer { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (optionsBuilder.IsConfigured) return;
@@ -15,7 +17,10 @@ namespace EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts
             optionsBuilder.UseSqlServer(ConnectionString);
         }
 
-        public DbSet<Customer> Customer { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasIndex(c => c.Cpf).IsUnique();
+        }
 
     }
 }
