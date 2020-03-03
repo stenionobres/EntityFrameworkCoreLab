e6483faabf875b9300c49b5c1d9e376bc9873730
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-03 07:03:06 -0300

    Includes fields in Customer

--- EntityFrameworkCoreLab.Persistence/DataTransferObjects/Amazon/Customer.cs
+++ EntityFrameworkCoreLab.Persistence/DataTransferObjects/Amazon/Customer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon
 {
@@ -8,5 +8,15 @@ namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Birthday { get; set; }
+        
+        [Required]
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Cpf { get; set; }
     }
 }
