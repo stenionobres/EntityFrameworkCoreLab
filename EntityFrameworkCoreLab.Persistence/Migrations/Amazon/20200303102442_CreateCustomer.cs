d344222a3cd7f01e854d681ab013dc95b3d4ccc2
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-03 07:34:37 -0300

    Creates end Customer migration

---
+++ EntityFrameworkCoreLab.Persistence/Migrations/Amazon/20200303102442_CreateCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreLab.Persistence.Migrations.Amazon
+{
+    public partial class CreateCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 80, nullable: false),
+                    Birthday = table.Column<DateTime>(type: "date", nullable: false),
+                    Email = table.Column<string>(maxLength: 60, nullable: false),
+                    Cpf = table.Column<string>(maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_Cpf",
+                table: "Customer",
+                column: "Cpf",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
