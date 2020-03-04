30037e1f8ddd3b57213107eaa116beb46dcf1a0a
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-04 19:54:12 -0300

    Add Product migration to EbayDatabaseFirst database

--- EntityFrameworkCoreLab.Persistence/Migrations/Ebay/20200304225000_CreateProduct.Designer.cs
+++ EntityFrameworkCoreLab.Persistence/Migrations/Ebay/20200304225000_CreateProduct.Designer.cs
@@ -4,14 +4,16 @@ using EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkCoreLab.Persistence.Migrations.Ebay
 {
     [DbContext(typeof(EbayDatabaseFirstDbContext))]
-    partial class EbayDatabaseFirstDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200304225000_CreateProduct")]
+    partial class CreateProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
