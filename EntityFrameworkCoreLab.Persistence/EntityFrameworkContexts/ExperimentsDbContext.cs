c3107028f78462278562f1d4e4c31afc57ba4905
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-07 18:37:30 -0300

    Add new DbContext
    To manage a database for experiments with configurations of EFCore

---
+++ EntityFrameworkCoreLab.Persistence/EntityFrameworkContexts/ExperimentsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts
+{
+    public class ExperimentsDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=192.168.1.14,22331;Database=Experiments;User ID=sa;Password=sqlserver.252707;
+                                                  Encrypt=False;Trusted_Connection=False;Connection Timeout=3000;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
