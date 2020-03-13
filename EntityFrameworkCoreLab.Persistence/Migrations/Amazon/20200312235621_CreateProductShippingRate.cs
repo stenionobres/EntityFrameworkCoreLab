ff56fe7a1c42d3cb3d6df05b3cde31abb441af3d
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-12 21:13:32 -0300

    Create ProductShippingRate DataTransferObject and migration

---
+++ EntityFrameworkCoreLab.Persistence/Migrations/Amazon/20200312235621_CreateProductShippingRate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreLab.Persistence.Migrations.Amazon
+{
+    public partial class CreateProductShippingRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductShippingRate",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false),
+                    ShippingRateId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductShippingRate", x => new { x.ProductId, x.ShippingRateId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductShippingRate");
+        }
+    }
+}
