3281ecc000a816c790821e538ef912658d944844
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-13 20:10:55 -0300

    Add ProductShippingRate (N x N) relationship migration on EbayDatabaseFirst

---
+++ EntityFrameworkCoreLab.Persistence/Migrations/Ebay/20200313230345_ProductShippingRateRelationship.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreLab.Persistence.Migrations.Ebay
+{
+    public partial class ProductShippingRateRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductShippingRate_ShippingRateId",
+                table: "ProductShippingRate",
+                column: "ShippingRateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductShippingRate_Product_ProductId",
+                table: "ProductShippingRate",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductShippingRate_ShippingRate_ShippingRateId",
+                table: "ProductShippingRate",
+                column: "ShippingRateId",
+                principalTable: "ShippingRate",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductShippingRate_Product_ProductId",
+                table: "ProductShippingRate");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductShippingRate_ShippingRate_ShippingRateId",
+                table: "ProductShippingRate");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductShippingRate_ShippingRateId",
+                table: "ProductShippingRate");
+        }
+    }
+}
