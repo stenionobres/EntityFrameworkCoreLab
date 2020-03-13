e9fb699f798b02a50898d218a114707e5388dcf8
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-13 10:35:30 -0300

    Add CustomerCart (1 x N) relationship migration on EbayDatabaseFirst

---
+++ EntityFrameworkCoreLab.Persistence/Migrations/Ebay/20200313133117_CustomerCartRelationship.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreLab.Persistence.Migrations.Ebay
+{
+    public partial class CustomerCartRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_CustomerId",
+                table: "Cart",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_Customer_CustomerId",
+                table: "Cart",
+                column: "CustomerId",
+                principalTable: "Customer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_Customer_CustomerId",
+                table: "Cart");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cart_CustomerId",
+                table: "Cart");
+        }
+    }
+}
