5a400eb833d214c4dc5a470c3eb7da14c38a2ae8
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-14 09:42:14 -0300

    Change Product schema on EbayDatabaseFirst

--- EntityFrameworkCoreLab.Persistence/DataTransferObjects/Ebay/Product.cs
+++ EntityFrameworkCoreLab.Persistence/DataTransferObjects/Ebay/Product.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay
 {
+    [Table("Product", Schema = "common")]
     public class Product
     {
         public int Id { get; set; }
