487b270238e303efeee53d54bc49e32d25069b96
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-03-14 08:51:25 -0300

    Change ProductShippingRate schema on AmazonCodeFirst

--- EntityFrameworkCoreLab.Persistence/DataTransferObjects/Amazon/ProductShippingRate.cs
+++ EntityFrameworkCoreLab.Persistence/DataTransferObjects/Amazon/ProductShippingRate.cs
@@ -1,6 +1,8 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon
 {
+    [Table("ProductShippingRate", Schema = "common")]
     public class ProductShippingRate
     {
         public int ProductId { get; set; }
---
+++ EntityFrameworkCoreLab.Persistence/Migrations/Amazon/20200314114708_ChangeProductShippingRateSchema.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreLab.Persistence.Migrations.Amazon
+{
+    public partial class ChangeProductShippingRateSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "ProductShippingRate",
+                newName: "ProductShippingRate",
+                newSchema: "common");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "ProductShippingRate",
+                schema: "common",
+                newName: "ProductShippingRate");
+        }
+    }
+}
