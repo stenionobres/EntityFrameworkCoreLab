bb4e1fa7ca9a6d430679bad5a5ef95f1e20a2646
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-05-02 08:04:17 -0300

    Add transaction lab scenario without transaction and SaveChanges after

---
+++ EntityFrameworkCoreLab.Application/Process/TransactionLabProcess.cs
@@ -0,0 +1,35 @@
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay;
+using EntityFrameworkCoreLab.Persistence.Mappers.Transaction;
+using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkCoreLab.Application.Process
+{
+    public class TransactionLabProcess
+    {
+        public void InsertAddressWithAddWithoutTransaction()
+        {
+            var ebayTransactionLabMapper = new EbayTransactionLabMapper();
+
+            ebayTransactionLabMapper.CleanAddressData();
+
+            var address = new List<Address>()
+            {
+                new Address() { Street = "5000 Gandy Street", ZipPostCode = "13057", City = "East Syracuse" },
+                new Address() { Street = "354 Cemetery Street", ZipPostCode = "95004", City = "Aromas" },
+                new Address() { Street = "2031 Reppert Coal Road", ZipPostCode = "45232", City = "Cincinnati" } // Street larger than the field in the database 
+            };
+
+            try
+            {
+                ebayTransactionLabMapper.InsertAddressWithAddWithoutTransactionSaveChangesAfter(address);
+            }
+            catch (Exception ex)
+            {
+                // The records don't be inserted because que SaveChanges was used after the error record was added
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ EntityFrameworkCoreLab.Persistence/Mappers/Transaction/EbayTransactionLabMapper.cs
@@ -0,0 +1,50 @@
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay;
+using EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Persistence.Mappers.Transaction
+{
+    public class EbayTransactionLabMapper
+    {
+        public void CleanAddressData()
+        {
+            using (var ebayDatabaseFirstContext = new EbayDatabaseFirstDbContext())
+            {
+                ebayDatabaseFirstContext.Database.ExecuteSqlInterpolated($"delete from common.Address where Id > 3");
+                ebayDatabaseFirstContext.Database.ExecuteSqlInterpolated($"DBCC CHECKIDENT ('common.Address', RESEED, 3)");
+            }
+        }
+
+        public void CleanCustomerData()
+        {
+            using (var ebayDatabaseFirstContext = new EbayDatabaseFirstDbContext())
+            {
+                ebayDatabaseFirstContext.Database.ExecuteSqlInterpolated($"delete from common.Customer where Id > 5");
+                ebayDatabaseFirstContext.Database.ExecuteSqlInterpolated($"DBCC CHECKIDENT ('common.Customer', RESEED, 5)");
+            }
+        }
+
+        public void InsertAddressWithAddWithoutTransactionSaveChangesAfter(IEnumerable<Address> addresses)
+        {
+            using (var ebayDatabaseFirstContext = new EbayDatabaseFirstDbContext())
+            {
+                foreach (var address in addresses)
+                {
+                    InsertAddressWithAdd(ebayDatabaseFirstContext, address);
+                }
+                
+                ebayDatabaseFirstContext.SaveChanges();
+            }
+        }
+
+
+
+        private void InsertAddressWithAdd(EbayDatabaseFirstDbContext ebayDatabaseFirstContext, Address address)
+        {
+            ebayDatabaseFirstContext.Address.Add(address);
+        }
+    }
+}
