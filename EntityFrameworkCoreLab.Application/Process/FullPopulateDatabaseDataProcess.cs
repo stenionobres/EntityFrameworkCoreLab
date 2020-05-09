bf74974411d533dc9f6f74111fa8d5b42ea0841c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-05-09 10:49:28 -0300

    Create basic structure to full populate database with data

---
+++ EntityFrameworkCoreLab.Application/Process/FullPopulateDatabaseDataProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Application.Process
+{
+    public class FullPopulateDatabaseDataProcess
+    {
+        public void FullPopulateDatabaseWithFakeData()
+        {
+
+        }
+    }
+}
---
+++ EntityFrameworkCoreLab.Persistence/Mappers/PopulateData/FullPopulateDatabaseDataMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Persistence.Mappers.PopulateData
+{
+    public class FullPopulateDatabaseDataMapper
+    {
+        public void CleanDataOfAllTables()
+        {
+
+        }
+
+        public void FullPopulateDatabase()
+        {
+
+        }
+    }
+}
