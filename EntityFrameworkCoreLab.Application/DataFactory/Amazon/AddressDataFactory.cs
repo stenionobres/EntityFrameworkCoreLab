3dbb26a081396c4033c288548dd2b2d2d188472f
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-05-09 12:04:14 -0300

    Create Amazon Address Factory
    To helps populate database

---
+++ EntityFrameworkCoreLab.Application/DataFactory/Amazon/AddressDataFactory.cs
@@ -0,0 +1,57 @@
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon;
+using FizzWare.NBuilder;
+using System.Collections.Generic;
+
+namespace EntityFrameworkCoreLab.Application.DataFactory.Amazon
+{
+    public class AddressDataFactory
+    {
+        public static IEnumerable<Address> Make(int quantity)
+        {
+            var address = Builder<Address>.CreateListOfSize(quantity)
+                                          .All()
+                                          .With(a => a.Id = 0)
+                                          .With(a => a.Street = GetStreet())
+                                          .With(a => a.ZipPostCode = GetZipCode())
+                                          .With(a => a.City = GetCity())
+                                          .Build();
+            return address;
+        }
+
+        private static string GetStreet()
+        {
+            var street = Faker.Address.StreetAddress();
+
+            if (street.Length > 20)
+            {
+                return street.Substring(0, 20);
+            }
+
+            return street;
+        }
+
+        private static string GetZipCode()
+        {
+            var zipCode = Faker.Address.ZipCode();
+
+            if (zipCode.Length > 8)
+            {
+                return zipCode.Substring(0, 8);
+            }
+
+            return zipCode;
+        }
+
+        private static string GetCity()
+        {
+            var city = Faker.Address.City();
+
+            if (city.Length > 20)
+            {
+                return city.Substring(0, 8);
+            }
+
+            return city;
+        }
+    }
+}
