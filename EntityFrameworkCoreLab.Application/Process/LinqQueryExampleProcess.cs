87ebaecbaac86cd08819710e99c2e9a6afacc771
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-05-24 16:58:51 -0300

    Add Linq Query Example using INNER JOIN

---
+++ EntityFrameworkCoreLab.Application/Process/LinqQueryExampleProcess.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon;
+using EntityFrameworkCoreLab.Persistence.Mappers.Query;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreLab.Application.Process
+{
+    public class LinqQueryExampleProcess
+    {
+        public IEnumerable<Customer> GetCustomersWithAddressAndCartsBasedInInnerJoin()
+        {
+            var customers = new LinqQueryExampleMapper().GetCustomersWithAddressAndCartsBasedInInnerJoin();
+
+            return customers;
+        }
+    }
+}
---
+++ EntityFrameworkCoreLab.Persistence/Mappers/Query/LinqQueryExampleMapper.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Amazon;
+using EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityFrameworkCoreLab.Persistence.Mappers.Query
+{
+    public class LinqQueryExampleMapper
+    {
+        public IEnumerable<Customer> GetCustomersWithAddressAndCartsBasedInInnerJoin()
+        {
+            using (var amazonCodeFirstContext = new AmazonCodeFirstDbContext())
+            {
+                var query = from customer in amazonCodeFirstContext.Customer
+                            join cart in amazonCodeFirstContext.Cart
+                                on customer.Id equals cart.CustomerId
+                            join address in amazonCodeFirstContext.Address
+                                on customer.AddressId equals address.Id
+                            select new
+                            {
+                                customer, address, cart
+                            };
+
+                var data = query.ToList();
+
+                return data.Select(d => d.customer);
+            }
+        }
+    }
+}
