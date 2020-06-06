716efc0ef2d26431f108995ec3b9ff41a4fc92bd
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-06-06 17:25:10 -0300

    Change Auditable to abstract class
    To use the [Required] annotation without change the entity class

--- EntityFrameworkCoreLab.Persistence/Auditing/Auditable.cs
+++ EntityFrameworkCoreLab.Persistence/Auditing/Auditable.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EntityFrameworkCoreLab.Persistence.Auditing
 {
-    interface IAuditable
+    public abstract class Auditable
     {
+        [Required]
         public string CreatedBy { get; set; }
+
+        [Required]
         public DateTime CreatedOn { get; set; }
+
+        [Required]
         public string UpdatedBy { get; set; }
+
+        [Required]
         public DateTime UpdatedOn { get; set; }
+
         public string DeletedBy { get; set; }
         public DateTime? DeletedOn { get; set; }
     }
