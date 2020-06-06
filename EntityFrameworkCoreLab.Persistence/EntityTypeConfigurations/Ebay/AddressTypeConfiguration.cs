26e5b0fda510ab14b5a783f561df61794d5c70f2
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-06-06 18:02:34 -0300

    Change Address Auditing migration
    To use getdate() to avoid new changes in another migrations

--- EntityFrameworkCoreLab.Persistence/EntityTypeConfigurations/Ebay/AddressTypeConfiguration.cs
+++ EntityFrameworkCoreLab.Persistence/EntityTypeConfigurations/Ebay/AddressTypeConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay;
 
@@ -10,13 +9,12 @@ namespace EntityFrameworkCoreLab.Persistence.EntityTypeConfigurations.Ebay
         public void Configure(EntityTypeBuilder<Address> builder)
         {
             var defaultUserId = "some_id_of_user";
-            var defaultAuditingDate = DateTime.Now;
 
             builder.Property(c => c.CreatedBy).HasDefaultValue(defaultUserId);
-            builder.Property(c => c.CreatedOn).HasDefaultValue(defaultAuditingDate);
+            builder.Property(c => c.CreatedOn).HasDefaultValueSql("getdate()");
 
             builder.Property(c => c.UpdatedBy).HasDefaultValue(defaultUserId);
-            builder.Property(c => c.UpdatedOn).HasDefaultValue(defaultAuditingDate);
+            builder.Property(c => c.UpdatedOn).HasDefaultValueSql("getdate()");
         }
     }
 }
---
+++ EntityFrameworkCoreLab.Persistence/Migrations/Ebay/20200606205821_AddAddressAuditing.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreLab.Persistence.Migrations.Ebay
+{
+    public partial class AddAddressAuditing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                schema: "common",
+                table: "Address",
+                nullable: false,
+                defaultValue: "some_id_of_user");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "common",
+                table: "Address",
+                nullable: false,
+                defaultValueSql: "getdate()");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                schema: "common",
+                table: "Address",
+                nullable: false,
+                defaultValue: "some_id_of_user");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                schema: "common",
+                table: "Address",
+                nullable: false,
+                defaultValueSql: "getdate()");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                schema: "common",
+                table: "Address",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                schema: "common",
+                table: "Address",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "common",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                schema: "common",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                schema: "common",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                schema: "common",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                schema: "common",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                schema: "common",
+                table: "Address");
+        }
+    }
+}
