c9046ba2af49bd35be40dc3bf9ecdf356ede6281
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-06-06 16:06:47 -0300

    Add auditing interface
    To be used in some entities for data auditing

---
+++ EntityFrameworkCoreLab.Persistence/Auditing/IAuditable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EntityFrameworkCoreLab.Persistence.Auditing
+{
+    interface IAuditable
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
