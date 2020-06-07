f8827301142b0f1b23418ae083ead414064f1267
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-06-07 09:42:48 -0300

    Add Address insert auditing

---
+++ EntityFrameworkCoreLab.Application/Process/AuditingProcess.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using EntityFrameworkCoreLab.Persistence.Mappers.Auditing;
+using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay;
+
+namespace EntityFrameworkCoreLab.Application.Process
+{
+    public class AuditingProcess
+    {
+        public void InsertAddressesWithAuditing()
+        {
+            var ebayAddressAuditingMapper = new EbayAddressAuditingMapper();
+            var addresses = new List<Address>();
+
+            ebayAddressAuditingMapper.CleanAddressData();
+
+            var address01 = new Address()
+            {
+                Street = "4035 Hermiston Vista",
+                ZipPostCode = "04826-03",
+                City = "South Monserrate"
+            };
+
+            var address02 = new Address()
+            {
+                Street = "00886 Klein Road",
+                ZipPostCode = "66822-01",
+                City = "Lake Dave"
+            };
+
+            addresses.Add(address01);
+            addresses.Add(address02);
+
+            ebayAddressAuditingMapper.InsertAddressesWithAuditing(addresses);
+        }
+    }
+}
---
+++ EntityFrameworkCoreLab.Persistence/Mappers/Auditing/EbayAddressAuditingMapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts;
+using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Ebay;
+
+namespace EntityFrameworkCoreLab.Persistence.Mappers.Auditing
+{
+    public class EbayAddressAuditingMapper
+    {
+        public void CleanAddressData()
+        {
+            using (var ebayDatabaseFirstDbContext = new EbayDatabaseFirstDbContext())
+            {
+                ebayDatabaseFirstDbContext.Database.ExecuteSqlInterpolated($"delete from common.Address where Id > 3");
+                ebayDatabaseFirstDbContext.Database.ExecuteSqlInterpolated($"DBCC CHECKIDENT ('common.Address', RESEED, 3)");
+            }
+        }
+
+        public void InsertAddressesWithAuditing(IEnumerable<Address> addresses)
+        {
+            using (var ebayDatabaseFirstDbContext = new EbayDatabaseFirstDbContext())
+            {
+                ebayDatabaseFirstDbContext.Address.AddRange(addresses);
+                ebayDatabaseFirstDbContext.SaveChanges();
+            }
+        }
+    }
+}
