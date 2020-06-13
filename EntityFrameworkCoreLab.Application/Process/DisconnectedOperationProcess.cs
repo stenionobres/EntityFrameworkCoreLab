ad39a161fb5bef45963660444eff112a733e6999
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-06-13 10:09:16 -0300

    Add disconnected OneToOne insert example

---
+++ EntityFrameworkCoreLab.Application/Process/DisconnectedOperationProcess.cs
@@ -0,0 +1,33 @@
+﻿using EntityFrameworkCoreLab.Persistence.Mappers.DisconnectedOperation;
+using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Experiments.OneToOneRelation;
+
+namespace EntityFrameworkCoreLab.Application.Process
+{
+    public class DisconnectedOperationProcess
+    {
+        public void InsertEntitiesWithOneToOneRelationship()
+        {
+            var disconnectedInsertMapper = new DisconnectedInsertMapper();
+
+            disconnectedInsertMapper.CleanAllRecordsFromPrincipalAndDependentEntitiesByConventionOTO();
+
+            var firstPrincipalEntityByConventionOTO = new PrincipalEntityByConventionOTO()
+            {
+                FirstProperty = 100,
+                SecondProperty = "example that will add the two entities",
+                DependentEntityByConventionOTO = new DependentEntityByConventionOTO() { FirstProperty = 50, SecondProperty = 155.67m }
+            };
+
+            new DisconnectedInsertMapper().InsertEntitiesWithOneToOneRelationship(firstPrincipalEntityByConventionOTO);
+
+            var secondPrincipalEntityByConventionOTO = new PrincipalEntityByConventionOTO()
+            {
+                FirstProperty = 200,
+                SecondProperty = "example that will add the second principal entity and reuse the dependent entity existing",
+                DependentEntityByConventionOTO = new DependentEntityByConventionOTO() { Id = 1 }
+            };
+
+            new DisconnectedInsertMapper().InsertEntitiesWithOneToOneRelationship(secondPrincipalEntityByConventionOTO);
+        }
+    }
+}
---
+++ EntityFrameworkCoreLab.Persistence/Mappers/DisconnectedOperation/DisconnectedInsertMapper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCoreLab.Persistence.EntityFrameworkContexts;
+using EntityFrameworkCoreLab.Persistence.DataTransferObjects.Experiments.OneToOneRelation;
+
+namespace EntityFrameworkCoreLab.Persistence.Mappers.DisconnectedOperation
+{
+    public class DisconnectedInsertMapper
+    {
+        public void CleanAllRecordsFromPrincipalAndDependentEntitiesByConventionOTO()
+        {
+            using (var experimentsDbContext = new ExperimentsDbContext())
+            {
+                experimentsDbContext.Database.ExecuteSqlInterpolated($"delete from PrincipalEntityByConventionOTO");
+                experimentsDbContext.Database.ExecuteSqlInterpolated($"DBCC CHECKIDENT ('PrincipalEntityByConventionOTO', RESEED, 0)");
+
+                experimentsDbContext.Database.ExecuteSqlInterpolated($"delete from DependentEntityByConventionOTO");
+                experimentsDbContext.Database.ExecuteSqlInterpolated($"DBCC CHECKIDENT ('DependentEntityByConventionOTO', RESEED, 0)");
+
+                experimentsDbContext.SaveChanges();
+            }
+        }
+
+        public void InsertEntitiesWithOneToOneRelationship(PrincipalEntityByConventionOTO principalEntityByConventionOTO)
+        {
+            using (var experimentsDbContext = new ExperimentsDbContext())
+            {
+                experimentsDbContext.DependentEntityByConventionOTO.Attach(principalEntityByConventionOTO.DependentEntityByConventionOTO);
+                experimentsDbContext.PrincipalEntityByConventionOTO.AddRange(principalEntityByConventionOTO);
+                experimentsDbContext.SaveChanges();
+            }
+        }
+
+    }
+}
